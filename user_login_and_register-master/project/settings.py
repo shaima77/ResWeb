22591ed66f34034c766b4ef4d46b7f97175835fb
shaima77 <93545602+shaima77@users.noreply.github.com>
2022-10-30 00:44:47 +0300

    Update settings.py

--- user_login_and_register-master/project/settings.py
+++ user_login_and_register-master/project/settings.py
@@ -149,8 +149,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ah14701@hotmail.com'
-EMAIL_HOST_PASSWORD = 'oqumlspncktjtgmj'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
